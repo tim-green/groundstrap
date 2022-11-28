db790b179614e69ece4bfb69eebcfc679c7597dc
Tim <github@timgreen.ws>
2022-11-28 14:28:00 +1100

    indented template parts

--- global-templates/hero.php
+++ global-templates/hero.php
@@ -14,9 +14,9 @@ if ( is_active_sidebar( 'hero' ) || is_active_sidebar( 'statichero' ) || is_acti
 	<div class="wrapper" id="wrapper-hero">
 
 		<?php
-		get_template_part( 'sidebar-templates/sidebar', 'hero' );
-		get_template_part( 'sidebar-templates/sidebar', 'herocanvas' );
-		get_template_part( 'sidebar-templates/sidebar', 'statichero' );
+			get_template_part( 'sidebar-templates/sidebar', 'hero' );
+			get_template_part( 'sidebar-templates/sidebar', 'herocanvas' );
+			get_template_part( 'sidebar-templates/sidebar', 'statichero' );
 		?>
 
 	</div>
